f6fc43d0ad87d3d73bfa21d442cbfe0580fa00bd
Marcus Vinicius <vmvini@hotmail.com>
2016-06-26 15:42:56 -0300

    adding marks

--- prototypes/maps_facade.js
+++ prototypes/maps_facade.js
@@ -14,6 +14,72 @@ HarassmentMap.prototype.createMap = function(divId){
 }
 
 
+/*
+	marks
+		mark
+			lat
+			lng
+			title
+*/
+HarassmentMap.prototype.loadMarks = function(marks){
+
+	var map = this.map;
+	console.log("vetor marks: " + marks.length);
+	console.log(marks[0]);
+
+	var i = 0;
+
+	marks.forEach(function(mark){
+
+		setTimeout(function() {
+
+    		addMarker({
+    			map: map,
+				pos: { lat: mark.lat, lng: mark.lng },
+				title: mark.title
+    		});
+
+    	}, ++i * 200);
+
+	});
+
+
+}
+
+
+
+
+
+HarassmentMap.prototype.createMark = function(){
+
+	var map = this.map;
+
+	/*
+	marcadores
+		estupro, assedio, violencia
+	*/
+
+	this.map.addListener('click', function(e){
+		
+		console.log("lat: ", e.latLng.lat());
+		console.log("lng: ", e.latLng.lng());
+		var latlng = {lat: e.latLng.lat(), lng: e.latLng.lng()};
+		
+		var markProps = {
+			map: map,
+			pos: latlng,
+			title: "title"
+		};
+
+		var marker = addMarker(markProps);
+
+
+
+	});
+
+}
+
+
 HarassmentMap.prototype.createInfoWindow = function(){
 
 	this.infoWindow = new google.maps.InfoWindow({map: this.map});
@@ -47,5 +113,41 @@ HarassmentMap.prototype.goToUserLocation = function(success, error){
 	    // Browser doesn't support Geolocation
 	    error();
   	}
+
+}
+
+
+
+/*
+		markProps
+			map
+			pos
+			animation - null
+			title
+		
+		markClick - callback for click event
+
+	*/
+function addMarker(markProps, markClick ){
+
+	var marker = new google.maps.Marker({
+		position: markProps.pos,
+		map:markProps.map,
+		animation: google.maps.Animation.DROP,
+		title:markProps.title
+	});
+
+	marker.addListener('click', function(){
+		if (marker.getAnimation() !== null) {
+			marker.setAnimation(null);
+		} else {
+			marker.setAnimation(google.maps.Animation.BOUNCE);
+		}
+		
+		if(markClick)
+			markClick();
+	});
+
+	return marker;
 
 }
