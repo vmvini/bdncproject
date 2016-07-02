27c48e2bd5179925a07a387fa090ccc9af74eea2
Marcus Vinicius <vmvini@hotmail.com>
2016-07-02 13:54:56 -0300

    connection event listeners

--- prototypes/webserver/app_server/models/db.js
+++ prototypes/webserver/app_server/models/db.js
@@ -11,3 +11,32 @@ MongoDB.on('error', function(err) {
 MongoDB.once('open', function() {
   console.log("mongodb connection open");
 });
+
+var dbShutdown = function(msg, callback){
+	MongoDB.close(function(){
+		console.log("Mongoose desconectado: " + msg);
+		callback();
+	});
+}
+
+//escutando sinal emitido por nodemon quando app é fechado 
+process.once('SIGUSR2', function(){
+	dbShutdown('nodemon restart', function(){
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
+
+//escutando sinal emitido pela aplicação quando app é fechado
+process.on('SIGINT', function(){
+	dbShutdown('app termination', function(){
+		process.exit(0);
+	});
+});
+
+
+//escutando sinal emitido pelo heroku quando app é fechado
+process.on('SIGTERM', function(){
+	dbShutdown('Heroku app shutdown', function(){
+		process.exit(0);
+	});
+});
