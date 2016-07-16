3765ee61d38bf442e4d6ad882d5368f3fb289fe1
Marcus Vinicius <vmvini@hotmail.com>
2016-07-16 04:48:01 -0300

    angular app routes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
-app.use(express.static(path.join(__dirname, 'public/templated')));
+//app.use(express.static(path.join(__dirname, 'public/templated')));
 
 
 
