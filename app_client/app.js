24cd338b8de5cd3877b85d7a839dd6265096ca00
Marcus Vinicius <vmvini@hotmail.com>
2016-07-16 04:50:15 -0300

    right angular routes

--- app_client/app.js
+++ app_client/app.js
@@ -1,1 +1,15 @@
-angular.module('mapaAssedioApp', []);
+angular.module('mapaAssedioApp', ['ngRoute']);
+
+function config($routeProvider){
+	$routeProvider
+		.when('/', {
+
+			templateUrl: 'views/index.html'	
+
+		})
+		.otherwise({redirectTo: '/'});
+}
+
+angular
+	.module('mapaAssedioApp')
+	.config(['$routeProvider', config]);
